0527d81b9b01bdb3b3c9cb4104647306349a8218
Pavel Sher <pavel.sher@jetbrains.com>
2026-09-04 11:31:12 +0200

    if we matched a user account by username claim and this user account was not created by the OIDC plugin or it was created when the username sub was different, then we must check the verified email to ensure that this is exactly the user who can login
    + in addition, if we create a new user account we now set its email as verified

--- teamcity-oidc-auth-server/src/main/java/org/jetbrains/teamcity/oidc/auth/OidcAuthenticationScheme.java
+++ teamcity-oidc-auth-server/src/main/java/org/jetbrains/teamcity/oidc/auth/OidcAuthenticationScheme.java
@@ -192,12 +192,45 @@ public class OidcAuthenticationScheme extends HttpAuthenticationSchemeAdapter {
             checkEmailDomain(email, settings.getAllowedEmailDomains());
         }
 
+        String sub = idTokenClaims.getSub();
+
         // 9. Lookup or create user
         SUser user = userModel.findUserAccount(null, username);
+        if (user != null) {
+            String existingSub = ((UserEx) user).getAttribute(OidcConstants.OIDC_SUB_ATTRIBUTE);
+            if (!sub.equals(existingSub)) {
+                // we found some user account by username, but they either never logged in with OIDC auth or the sub claim has been changed
+                // instead of simply allowing them to login we need to ensure that we can match the user by the verified email
+                if (email == null) {
+                    Loggers.AUTH.info("OIDC: the user account is found by '" + username + "' but it was not created by the OIDC auth and there is no email claim available to match it by the verified email");
+                    throw new OidcAuthException("Cannot authenticate user with username '" + username + "' (ask system administrator for details)");
+                }
+
+                if (!Boolean.TRUE.equals(idTokenClaims.getEmailVerified())) {
+                    Loggers.AUTH.info("OIDC: the user account is found by '" + username + "' but it was not created by the OIDC auth and there is no verified email claim available");
+                    throw new OidcAuthException("Cannot authenticate user with username '" + username + "' (ask system administrator for details)");
+                }
+
+                String verifiedEmail = ((UserEx) user).getVerifiedEmail();
+                if (verifiedEmail == null) {
+                    Loggers.AUTH.info("OIDC: the user account is found by '" + username + "' but it does not have a verified email");
+                    throw new OidcAuthException("Cannot authenticate user with username '" + username + "' (ask system administrator for details)");
+                }
+
+                if (!email.equals(verifiedEmail)) {
+                    Loggers.AUTH.info("OIDC: the user account is found by '" + username + "' but its verified email '" + verifiedEmail + "' differs from the email claim: '" + email + "'");
+                    throw new OidcAuthException("Cannot authenticate user with username '" + username + "' (ask system administrator for details)");
+                }
+            }
+        }
+
         if (user == null && settings.isCreateUsersAutomatically()) {
             Loggers.AUTH.info("OIDC: auto-creating user '" + username + "'");
             String displayName = resolveClaim(settings.getDisplayNameClaim(), idTokenClaims, userInfo);
             user = userModel.createUserAccount(new NewUserAccount(username, displayName, email, null, Collections.emptyMap()));
+            if (email != null && Boolean.TRUE.equals(idTokenClaims.getEmailVerified())) {
+                ((UserEx)user).setEmailIsVerified(email);
+            }
         }
         if (user == null) {
             throw new OidcAuthException("User '" + username + "' not found and auto-creation is disabled");
@@ -209,7 +242,6 @@ public class OidcAuthenticationScheme extends HttpAuthenticationSchemeAdapter {
             syncGroups(user, groups, settings.isRemoveUnassignedGroups());
         }
 
-        String sub = idTokenClaims.getSub();
         ((UserEx) user).setAttribute(OidcConstants.OIDC_SUB_ATTRIBUTE, sub);
 
         // Store access token and user ID in session to support per-request token introspection
